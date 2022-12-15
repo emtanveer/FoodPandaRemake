9ecb8adffe181e5632c2674e2717a03e3eae9984
Sharik Ali <sharik.ali@sikander.jp>
2022-12-15 15:40:16 +0500

    Implemented location permission for both approximate and precise location and handle show rationale UI when user deny permission
    Signed-off-by: Sharik Ali <sharik.ali@sikander.jp>

--- app/src/main/java/com/fpremake/screens_pre_login/screen_landing_location/presentation/LandingLocationScreen.kt
+++ app/src/main/java/com/fpremake/screens_pre_login/screen_landing_location/presentation/LandingLocationScreen.kt
@@ -1,32 +1,101 @@
 package com.fpremake.screens_pre_login.screen_landing_location.presentation
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import com.fpremake.R
+import com.google.accompanist.permissions.*
 
+@RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun LandingLocationScreen(navController: NavHostController?) {
     LandingLocationUiContent()
 }
 
+@RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun LandingLocationUiContent() {
+
+    var showFineLocationRationaleMessage by remember {
+        mutableStateOf(false)
+    }
+    var showCoarseLocationRationaleMessage by remember {
+        mutableStateOf(false)
+    }
+
+    val context = LocalContext.current
+    val activityContext = LocalContext.current as Activity
+
+    val permission = arrayListOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+    )
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            when {
+                //getOrDefault work only in Api 24+
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    Log.d("TAG", "ACCESS_COARSE_LOCATION: Granted")
+                    //Todo Approximate Permission Granted
+                }
+
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    Log.d("TAG", "ACCESS_FINE_LOCATION: Granted")
+                    //Todo Precise Permission Granted
+                }
+
+                /**
+                 * In an educational UI, explain to the user why your app requires this
+                 * permission for a specific feature to behave as expected, and what
+                 * features are disabled if it's declined. In this UI, include a
+                 * "cancel" or "no thanks" button that lets the user continue
+                 * using your app without granting the permission.
+                 */
+                ActivityCompat.shouldShowRequestPermissionRationale(activityContext,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) -> { showCoarseLocationRationaleMessage = true }
+
+                ActivityCompat.shouldShowRequestPermissionRationale(activityContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION) -> { showFineLocationRationaleMessage = true }
+
+                else -> {
+                    /**
+                     * if both permission deny 2 time then it will not show again.
+                     * then user need to manually allow this location permission for both,
+                     * Precise and Approximate
+                     */
+                    Log.d("TAG", "No Permission")
+                }
+            }
+        }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -40,7 +109,9 @@ fun LandingLocationUiContent() {
             Image(
                 painter = painterResource(id = R.drawable.location_logo),
                 contentDescription = "location Logo",
-                modifier = Modifier.size(100.dp,100.dp).background(Color.Transparent),
+                modifier = Modifier
+                    .size(100.dp, 100.dp)
+                    .background(Color.Transparent),
                 contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.size(40.dp))
@@ -59,10 +130,16 @@ fun LandingLocationUiContent() {
             Text(text = "restaurants and shops near you and receive", fontSize = 16.sp)
             Text(text = "more accurate delivery.", fontSize = 16.sp)
         }
+
         Column(verticalArrangement = Arrangement.Bottom) {
             Button(modifier = Modifier
                 .fillMaxWidth(),
-                onClick = { },
+                onClick = {
+                    requestAndHandleLocationPermission(
+                        context = context,
+                        launcher = launcher,
+                    )
+                },
                 shape = RoundedCornerShape(8.dp)) {
                 Text(
                     modifier = Modifier.padding(10.dp),
@@ -79,11 +156,109 @@ fun LandingLocationUiContent() {
                     text = "Enter my location"
                 )
             }
+            if (showCoarseLocationRationaleMessage) {
+                ShowAlertDialogForRationaleUIContent(
+                    title = "Allow Approximate Location Permission",
+                    message = "Allow Foodpanda to access Approximate location, this lets you see near by places, " +
+                            "You can change this anytime in your device settings.",
+                    permissions = permission.toTypedArray(),
+                    launcher = launcher,
+                    closeDialogCallback = { showCoarseLocationRationaleMessage = false }
+                )
+            }
+            if (showFineLocationRationaleMessage) {
+                ShowAlertDialogForRationaleUIContent(
+                    title = "Allow Precise Location Permission",
+                    message = "Allow Foodpanda to access Precise location, " +
+                            "this will help you to see your current location & share your location with others, " +
+                            "You can change this anytime in your device settings.",
+                    permissions = permission.toTypedArray(),
+                    launcher = launcher,
+                    closeDialogCallback = { showFineLocationRationaleMessage = false }
+                )
+            }
         }
     }
 }
 
+/**
+ * Show alert dialog
+ *
+ * @param modifier              Default Modifier.
+ * @param title                 Title for alert dialog.
+ * @param message               Message for alert dialog.
+ * @param permissions           List of permissions for request.
+ * @param launcher              Launcher for request permission when user deny permission .
+ * @param closeDialogCallback   Callback when click on dialog action button & perform some operation, then close the dialog.
+ */
+@Composable
+fun ShowAlertDialogForRationaleUIContent(
+    modifier: Modifier = Modifier,
+    title: String,
+    message: String,
+    permissions: Array<String>,
+    launcher: ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>,
+    closeDialogCallback: () -> Unit,
+) {
+    AlertDialog(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Text(
+                text = title,
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        },
+        text = {
+            Text(text = message)
+        },
+        confirmButton = {
+            TextButton(onClick = {
+                launcher.launch(permissions)
+                closeDialogCallback()
+            }) {
+                Text("Request")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = closeDialogCallback) {
+                Text(text = "Cancel")
+            }
+        },
+        onDismissRequest = {},
+    )
+}
 
+private fun requestAndHandleLocationPermission(
+    context: Context,
+    launcher: ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>,
+) {
+    val isFineLocationGranted = ContextCompat.checkSelfPermission(context,
+        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    val isCoarseLocationGranted = ContextCompat.checkSelfPermission(context,
+        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    val permissions = arrayListOf<String>()
+
+    //if precise permission is not granted add permission for request
+    if (!isFineLocationGranted) {
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+    //if approximate permission is not granted add permission for request
+    if (!isCoarseLocationGranted) {
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+    }
+
+    if (permissions.isNotEmpty()) {
+        launcher.launch(permissions.toTypedArray())
+    }
+}
+
+
+@RequiresApi(Build.VERSION_CODES.N)
 @Preview(showBackground = true)
 @Composable
 fun PreviewLandingLocation() {
