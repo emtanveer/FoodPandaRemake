eb35171bcd6f72e335050b5a4c8e16cf02cb4f61
Muhammad Tanveer <m.tanveer@sikander.jp>
2023-01-10 16:42:14 +0500

    Completed Integrated Testing, kicked off Realm Unit Testing using MockK.
    Signed-off-by: Muhammad Tanveer <m.tanveer@sikander.jp>

--- app/src/androidTest/java/com/fpremake/kotlin/InsertAnUserAfterAuthentication.kt
+++ app/src/androidTest/java/com/fpremake/kotlin/InsertAnUserAfterAuthentication.kt
@@ -1,13 +1,11 @@
 package com.fpremake.kotlin
 
 import android.util.Log
-import com.fpremake.BuildConfig
 import com.fpremake.Expectation
 import com.fpremake.RealmTesting
 import com.fpremake.screens_post_login.screen_dashboard.data.Child
 import com.fpremake.screens_post_login.screen_dashboard.data.Parent
 import com.fpremake.screens_post_login.screen_dashboard.data.User
-import com.fpremake.shared.Emojis
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.ext.query
@@ -15,62 +13,69 @@ import io.realm.kotlin.mongodb.App
 import io.realm.kotlin.mongodb.AppConfiguration
 import io.realm.kotlin.mongodb.Credentials
 import io.realm.kotlin.query.RealmResults
-import io.realm.kotlin.query.RealmSingleQuery
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
 
 class InsertAnUserAfterAuthentication : RealmTesting() {
+    @JvmField
+    val TAG = "authentication"
+
+    val REALM = "realmDB"
     private val appId = "fpremakeapp-jocbl"
+
     @Test
     fun authenticateUserAndSaveUserInformationToRealmDataBase() {
         val expectation = Expectation()
 
         var result: RealmResults<User>? = null
+
+        val testLatch = CountDownLatch(1)
+
         activity!!.runOnUiThread {
             val realm = Realm.open(getRealmConfig())
-
             val app = App.create(AppConfiguration.Builder(appId).build())
+
             runBlocking {
                 val user = app.login(Credentials.anonymous())
-                Log.d("User","Successfully logged in ${user.id}")
-            }
-            // :snippet-start:
-            try {
-                realm.writeBlocking {
-//                    this.copyToRealm(
-//                        User().apply {
-//                            this.firstName = "Tanveer"
-//                            this.isComplete = false
-//                            this.father_id = "2233"
-//                            this.emoji = Emojis.emojis?.get(7) ?: ""
-//                        }
-//                    )
-                    // get all items in the realm
-                    //val items: RealmResults<User> = userRepository.realmInstance.query<User>().find()
-                    result = this.query<User>().find()
-                    result?.forEachIndexed { index, user ->
-                        Log.e(
-                            "realm",
-                            "user # $index, ${user._id},${user.firstName},${user.father_id},${user.emoji}"
-                        )
+                Log.e(TAG, "user Access Token: ${user.accessToken}")
+                if (user.accessToken == null) {
+                    Log.e(TAG, "User Access Token not Found !!")
+                    expectation.fulfill()
+                }
+                else {
+                    // means user has token
+                    Log.e(TAG, "Successfully authenticated.")
+                    Log.e(TAG, "Successfully logged in ${user.id}")
+
+                    Log.e(REALM, "Successfully opened a realm at: ${realm.configuration.path}")
+                    //:snippet-start:
+                    try {
+                        realm.writeBlocking {
+                            result = this.query<User>().find()
+                            result?.forEachIndexed { index, user ->
+                                Log.e(
+                                    REALM,
+                                    "user # $index, ${user._id},${user.firstName},${user.father_id},${user.emoji}"
+                                )
+                            }
+
+                        }
+                        testLatch.countDown()
+
+                        expectation.fulfill()
+                    }
+                    // :snippet-end:
+                    catch (error: Exception) {
+                        expectation.fulfill()
                     }
                 }
-
-                Log.e("EXAMPLE", "Successfully opened a realm at: ${realm.configuration.path}")
-                expectation.fulfill()
-            }
-            // :snippet-end:
-            catch (error: Exception) {
-                expectation.fulfill()
             }
             realm.close()
         }
+
         expectation.await()
     }
-
 
     private fun getRealmConfig(): RealmConfiguration {
         // :snippet-start: configure-a-realm-local
--- app/src/test/java/com/fpremake/ExampleUnitTest.kt
+++ app/src/test/java/com/fpremake/ExampleUnitTest.kt
@@ -1,17 +1,78 @@
 package com.fpremake
 
+import android.util.Log
+import com.fpremake.screens_post_login.screen_dashboard.data.Child
+import com.fpremake.screens_post_login.screen_dashboard.data.Parent
+import com.fpremake.screens_post_login.screen_dashboard.data.User
+import com.fpremake.shared.Emojis
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 class ExampleUnitTest {
+    companion object {
+        // var _id: ObjectId = ObjectId.create()
+        //    var isComplete: Boolean = false
+        //    var firstName: String = ""
+        //    var emoji: String = ""
+        //    var father_id: String = ""
+        //    var event : String = "default" // partition key
+
+
+
+        //Mock data
+        fun buildUser() = User().apply {
+            this.firstName = "Sharik"
+            this.isComplete = true
+            this.father_id = "2201"
+            this.emoji = Emojis.emojis?.get(5) ?: ""
+        }
+    }
+   lateinit var realm: Realm.Companion
+
+    @Before
+    fun setUp() {
+        //REALM OBJECT WITH CONFIG
+
+        // :snippet-start: configure-a-realm-local
+        val config = RealmConfiguration.Builder(
+            schema =
+            setOf(
+                User::class,
+                Parent::class,
+                Child::class
+            )
+        )
+            .name("alternate-realm-test")
+            .compactOnLaunch()
+            .build()
+
+        realm.open(config)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        fun `verify correct username is retrieved`() = runTest {
+        val name = "Sharik"
+        runBlocking {
+            try {
+                realm.writeBlocking {
+                    result = this.query<User>().find()
+                }
+            } catch (e: IllegalAccessException) {
+
+            }
+        }
     }
 }
